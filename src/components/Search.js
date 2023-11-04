c49b3c65fc3100accc5a3117e7be767beacddda9
Rushi <rushikeshshelar.cs@gmail.com>
2023-11-05 00:36:31 +0530

    Adding DarkMode Functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { mockSearchResults } from '../constants/mock';
 import { XIcon, SearchIcon } from '@heroicons/react/solid';
 import SearchResults from './SearchResults';
+import ThemeContext from '../context/ThemeContext';
 
 const Search = () => {
     const [input, setInput] = useState('');
     const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
+
+    const { darkMode } = useContext(ThemeContext);
 
     const clear = () => {
         setInput('');
@@ -17,11 +20,15 @@ const Search = () => {
     };
 
     return (
-        <div className="flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200">
+        <div className={`flex items-center my-4 border-2 rounded-md relative z-50 w-96 ${darkMode
+            ? 'bg-gray-900 border-gray-800'
+            : ' bg-white border-neutral-200'}`}>
             <input
                 type="text"
                 value={input}
-                className='w-full px-4 py-2 focus:outline-none rounded-md'
+                className={`w-full px-4 py-2 focus:outline-none rounded-md ${darkMode
+                    ? 'bg-gray-900'
+                    : null}`}
                 placeholder='Search Stock'
                 onChange={(event) => {
                     setInput(event.target.value);
@@ -39,10 +46,10 @@ const Search = () => {
             )}
 
             <button onClick={updateBestMatches} className='h-8 w-8 bg-indigo-600 rounded-md flex justifu-cemter items-center m-1 p-2'>
-                <SearchIcon className='h-4 w-f fill-gray-100'/>
+                <SearchIcon className='h-4 w-f fill-gray-100' />
             </button>
 
-            {input && bestMatches.length > 0 ? <SearchResults results={bestMatches}/> : null}
+            {input && bestMatches.length > 0 ? <SearchResults results={bestMatches} /> : null}
         </div>
     )
 }
--- src/components/ThemeIcon.js
+++ src/components/ThemeIcon.js
@@ -1,10 +1,23 @@
 import { MoonIcon } from '@heroicons/react/solid'
-import React from 'react'
+import React, { useContext } from 'react'
+import ThemeContext from '../context/ThemeContext'
 
 const ThemeIcon = () => {
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  };
   return (
-    <button className='rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-28 shadow-lg '>
-        <MoonIcon className='h-6 w-6 cursor-point stroke-1 fill-none stroke-neutral-400'/>
+    <button
+      className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-28 shadow-lg ${darkMode
+          ? 'shadow-gray-700'
+          : null
+        }`}
+      onClick={toggleDarkMode}>
+      <MoonIcon className={`h-6 w-6 cursor-point stroke-1 fill-none  ${darkMode
+          ? 'fill-yellow-400 stroke-yello-400'
+          : 'fill-none stroke-neutral-400'
+        }`} />
     </button>
   )
 }
